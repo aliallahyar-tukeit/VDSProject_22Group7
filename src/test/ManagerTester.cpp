90a77cd98ef0cea3aa13e7103802d8177906e4c3
eloi <eloi.guihard.goujon@etu.imt-nord-europe.fr>
2022-12-14 19:26:00 +0100

    test: OR

--- src/test/ManagerTester.cpp
+++ src/test/ManagerTester.cpp
@@ -94,3 +94,48 @@ TEST_F(ManagerTester, fn_topVarIdForTrue) {
     ClassProject::BDD_ID topVariableIdForTrue = trueNode.top_var;
     EXPECT_EQ(topVariableIdForTrue, 1);
 }
+
+TEST_F(ManagerTester, fn_OR_TopVariable) { // Should be 2
+    ClassProject::BDD_ID createdA = manager->createVar("a"); // id is 2
+    ClassProject::BDD_ID createdB = manager->createVar("b"); // id is 3
+    ClassProject::BDD_ID createdNodeOR_AB = manager->or2(createdA, createdB); // id is 4
+    ClassProject::BDD_ID topVariableIdForOR = manager->topVar(createdNodeOR_AB);
+    EXPECT_EQ(topVariableIdForOR, 2);
+}
+
+TEST_F(ManagerTester, fn_OR_LowId) { // Should be 3
+    ClassProject::BDD_ID createdA = manager->createVar("a"); // id is 2
+    ClassProject::BDD_ID createdB = manager->createVar("b"); // id is 3
+    ClassProject::BDD_ID createdNodeOR_AB = manager->or2(createdA, createdB); // id is 4
+    ClassProject::BDD_ID lowIdForOR = manager->nodes.find(createdNodeOR_AB)->second.low;
+    EXPECT_EQ(lowIdForOR, 3);
+}
+
+TEST_F(ManagerTester, fn_OR_HighId) { // Should be 1
+    ClassProject::BDD_ID createdA = manager->createVar("a"); // id is 2
+    ClassProject::BDD_ID createdB = manager->createVar("b"); // id is 3
+    ClassProject::BDD_ID createdNodeOR_AB = manager->or2(createdA, createdB); // id is 4
+    ClassProject::BDD_ID highIdForOR = manager->nodes.find(createdNodeOR_AB)->second.high;
+    EXPECT_EQ(highIdForOR, 1);
+}
+
+TEST_F(ManagerTester, fn_OR_Three_Variables_LowId) { // Should be 4
+    ClassProject::BDD_ID createdA = manager->createVar("a"); // id is 2
+    ClassProject::BDD_ID createdB = manager->createVar("b"); // id is 3
+    ClassProject::BDD_ID createdC = manager->createVar("c"); // id is 4
+    ClassProject::BDD_ID createdNodeOR_AB = manager->or2(createdA, createdB); // id is 5
+    // auto creation of b + c => id is 6
+    ClassProject::BDD_ID createdNodeOR_AB_C = manager->or2(createdNodeOR_AB, createdC); // id is 7
+    ClassProject::BDD_ID lowIdForOR = manager->nodes.find(createdNodeOR_AB_C)->second.low;
+    EXPECT_EQ(lowIdForOR, 6); // => a low => b + c
+}
+
+TEST_F(ManagerTester, fn_OR_Three_Variables_HighId) { // Should be 1
+    ClassProject::BDD_ID createdA = manager->createVar("a"); // id is 2
+    ClassProject::BDD_ID createdB = manager->createVar("b"); // id is 3
+    ClassProject::BDD_ID createdC = manager->createVar("c"); // id is 4
+    ClassProject::BDD_ID createdNodeOR_AB = manager->or2(createdA, createdB); // id is 5
+    ClassProject::BDD_ID createdNodeOR_AB_C = manager->or2(createdNodeOR_AB, createdC); // id is 6
+    ClassProject::BDD_ID highIdForOR = manager->nodes.find(createdNodeOR_AB_C)->second.high;
+    EXPECT_EQ(highIdForOR, 1);
+}
